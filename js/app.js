d73d1ef5a7f3c5f60890df5cab05710193e29f04
Jesus Gonzalez <jesusglezt27@gmail.com>
2024-01-10 14:08:38 -0600

    done

--- js/app.js
+++ js/app.js
@@ -1,99 +1,103 @@
-const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const vaciarCarrito = document.querySelector('#vaciar-carrito')
 let articulosCarrito = [];
 
-cargarEventListeners();
-
-function cargarEventListeners() {
+cargarEventListeners()
+function cargarEventListeners(){
      listaCursos.addEventListener('click', agregarCurso);
 
-     carrito.addEventListener('click', eliminarCurso);
+     carrito.addEventListener('click', eliminarCurso)
 
-     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+     vaciarCarrito.addEventListener('click', ()=>{
+          articulosCarrito = [];
 
+          limpiarHTML();
+     })
+
+     document.addEventListener('DOMContentLoaded', ()=>{
+          articulosCarrito = JSON.parse(localStorage.getItem('articulosCarrito')) || []
+          carritoHTML()
+     })
 }
 
-
-
-
-function agregarCurso(e) {
+function agregarCurso(e){
      e.preventDefault();
-     if(e.target.classList.contains('agregar-carrito')) {
-          const curso = e.target.parentElement.parentElement;
-          leerDatosCurso(curso);
+     if(e.target.classList.contains('agregar-carrito')){
+          const cursoSeleccionado = e.target.parentElement.parentElement;
+          leerDatosCurso(cursoSeleccionado)
      }
 }
 
-function leerDatosCurso(curso) {
-     const infoCurso = {
-          imagen: curso.querySelector('img').src,
-          titulo: curso.querySelector('h4').textContent,
-          precio: curso.querySelector('.precio span').textContent,
-          id: curso.querySelector('a').getAttribute('data-id'), 
-          cantidad: 1
+
+function eliminarCurso(e){
+     if(e.target.classList.contains('borrar-curso')){
+          const cursoId = e.target.getAttribute('data-id');
+
+          articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId)
      }
-
-
-     if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
-          const cursos = articulosCarrito.map( curso => {
-               if( curso.id === infoCurso.id ) {
-                    curso.cantidad++;
-                    return curso;
-               } else {
-                    return curso;
-               }
-          })
-          articulosCarrito = [...cursos];
-     }  else {
-          articulosCarrito = [...articulosCarrito, infoCurso];
-     }
-
-
-     
 
      carritoHTML();
 }
 
-function eliminarCurso(e) {
-     e.preventDefault();
-     if(e.target.classList.contains('borrar-curso') ) {
-          const cursoId = e.target.getAttribute('data-id')
-          
-          articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+function leerDatosCurso(curso){
 
-          carritoHTML();
+     const infoCurso = {
+          imagen: curso.querySelector('img').src,
+          titulo: curso.querySelector('h4').textContent,
+          precio: curso.querySelector('p span').textContent,
+          id: curso.querySelector('a').getAttribute('data-id'),
+          cantidad: 1
      }
+
+     const existe = articulosCarrito.some(curso => curso.id === infoCurso.id)
+     if(existe){
+          const cursos = articulosCarrito.map( curso =>{
+               if(curso.id === infoCurso.id){
+                    curso.cantidad++;
+                    return curso;
+               } else{
+                    return curso;
+               }
+          })
+          articulosCarrito = [...cursos];
+     } else{
+          articulosCarrito = [...articulosCarrito, infoCurso]
+     }
+
+     carritoHTML()
 }
 
+function carritoHTML(){
 
-function carritoHTML() {
+     limpiarHTML()
 
-     vaciarCarrito();
-
-     articulosCarrito.forEach(curso => {
+     articulosCarrito.forEach( curso =>{
+          const {imagen,titulo,precio,id,cantidad} = curso;
           const row = document.createElement('tr');
           row.innerHTML = `
-               <td>  
-                    <img src="${curso.imagen}" width=100>
-               </td>
-               <td>${curso.titulo}</td>
-               <td>${curso.precio}</td>
-               <td>${curso.cantidad} </td>
-               <td>
-                    <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
-               </td>
-          `;
-          contenedorCarrito.appendChild(row);
-     });
+          <td><img src="${imagen}" style="width:200px"></td>
+          <td>${titulo}</td>
+          <td>${precio}</td>
+          <td>${cantidad}</td>
+          <td><a href="#" class="borrar-curso" data-id="${id}"> X </a></td>
+     `;
+
+     contenedorCarrito.appendChild(row)
+
+     })
+
+     sincronizarStorage()
 
 }
 
-function vaciarCarrito() {
-
-
-     while(contenedorCarrito.firstChild) {
-          contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+function limpiarHTML(){
+     while(contenedorCarrito.firstChild){
+     contenedorCarrito.removeChild(contenedorCarrito.firstChild)
      }
+}
+
+function sincronizarStorage(){
+     localStorage.setItem('articulosCarrito', JSON.stringify(articulosCarrito))
 }
